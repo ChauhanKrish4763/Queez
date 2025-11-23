81ba31711ff361f37c7be2b42edfefd774258e15
Arhaan-P <ironarhaan@gmail.com>
2025-11-23 13:03:11 +0530

    Removing 'backend' folder

--- backend/app/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-def generate_session_code(length: int = 6) -> str:
-    """Generate a unique alphanumeric session code"""
-    characters = string.ascii_uppercase + string.digits
-    return ''.join(secrets.choice(characters) for _ in range(length))
